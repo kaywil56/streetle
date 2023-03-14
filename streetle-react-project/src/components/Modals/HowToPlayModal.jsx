e0096a748f95e44423f6f9c920f6a70fddcbeb2f
Dane <soredt1@student.op.ac.nz>
2023-03-14 18:06:56 +1300

    updated the how to play modal with as much instrction as i could come up with

--- streetle-react-project/src/components/Modals/HowToPlayModal.jsx
+++ streetle-react-project/src/components/Modals/HowToPlayModal.jsx
@@ -14,7 +14,44 @@ const Modal = ({ setIsOpen }) => {
             <RiCloseLine style={{ marginBottom: "-3px" }} />
           </button>
           <div className="modalContent">
-            Guess which country you are in
+            <p>
+              <h3>Objective:</h3> 
+              Use the street view image to correctly guess which country in the world you are in.
+            </p>
+            <p>
+              <h3>Gameplay:</h3> 
+              - Look closely at the street view image to gather clues about the countries location.
+              <br />
+              - Type your guess into the input area at the bottom of the screen.
+              <br />
+              - Click the "Guess" button to submit your guess.
+              <br />
+              - You have 5 guesses to correctly guess the country and win the round.
+              <br />
+              - Each incorrect guess outputs useful data into the legend in the top right corner of the screen.
+              <br />
+              - The legend displays the following information:
+              <ul>
+                <li>The name of the country you guessed.</li>
+                <li>The direction from your guessed country to the correct country.</li>
+                <li>The distance between your guessed country and the correct country in kilometers.</li>
+                <li>The first icon represents the continent (Is your guess in the correct continent?)</li>
+                <li>The second icon represents bordering countries (Does your guess share a border with the correct country?)</li>
+                <li>The third icon represents the hemisphere (Have you guessed in the correct hemisphere?)</li>
+              </ul>
+              - Icons will turn green if your guess matches the attributes of the icons.
+            </p>
+            <p>
+              <h3>Tips:</h3> 
+              - Pay close attention to the details in the street view image.
+              <br />
+              - Use the legend to guide your next guess.
+              <br />
+              - Take note of the information displayed in the legend to help you make more informed guesses.
+            </p>
+            <p>
+              <h3 className="footNote">Good luck and have fun playing Streetle!</h3>
+            </p>
           </div>
           <div className="modalActions">
             <div className="actionsContainer">
