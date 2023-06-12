ff4990b6087f7b6ebaf4bf988d8d556ae44ce582
kaylem williams <kaywil56>
2023-06-12 16:46:41 +1200

    Add better indicator of winning or losing

--- streetle-react-project/src/components/Modals/Summary.jsx
+++ streetle-react-project/src/components/Modals/Summary.jsx
@@ -1,47 +1,51 @@
 import "./Summary.css";
 
 const Summary = ({ didWin, score, country, totalGuesses }) => {
-    return (
-      <>
-        <div className="overlay"></div>
-        <div className="summary-modal">
-          <div className="summary-modalHeader">
-            <h1>Summary</h1>
-          </div>
-          <div className="summary-modalContent">
-            {didWin ? <>
-              <p>
-              You guessed <b>{country}</b> in <b>{score}</b> guesses
-            </p>
-            <ol>
-              {totalGuesses.map((guess, idx) => {
-                return (
-                  <li
-                    className="summary-item"
-                    key={`guess-${idx}`}
-                  >
-                    {guess.guess}
-                  </li>
-                );
-              })}
-            </ol>
-            </> : <><p>The correct country was <b>{country}</b></p>
-              <ol>
-              {totalGuesses.map((guess, idx) => {
-                return (
-                  <li
-                    className="summary-item"
-                    key={`guess-${idx}`}
-                  >
-                    {guess.guess}
-                  </li>
-                );
-              })}
-            </ol></>}
-          </div>
+  return (
+    <>
+      <div className="overlay"></div>
+      <div className="summary-modal">
+        <div className="summary-modalHeader">
+          <h1>Summary</h1>
         </div>
-      </>
-    );
+        <div className="summary-modalContent">
+          {didWin ? (
+            <>
+              <p className="outcome-text">Congrats!</p>
+              <p>
+                You guessed <b>{country}</b> in <b>{score}</b> guesses
+              </p>
+              <ol>
+                {totalGuesses.map((guess, idx) => {
+                  return (
+                    <li className="summary-item" key={`guess-${idx}`}>
+                      {guess.guess}
+                    </li>
+                  );
+                })}
+              </ol>
+            </>
+          ) : (
+            <>
+             <p className="outcome-text">Almost!</p>
+              <p>
+                The correct country was <b>{country}</b>
+              </p>
+              <ol>
+                {totalGuesses.map((guess, idx) => {
+                  return (
+                    <li className="summary-item" key={`guess-${idx}`}>
+                      {guess.guess}
+                    </li>
+                  );
+                })}
+              </ol>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Summary;
