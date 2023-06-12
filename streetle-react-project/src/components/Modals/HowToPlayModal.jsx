068ec828f1b692b45a25cf6abeac6e693e11a886
kaylem williams <kaywil56>
2023-06-12 16:38:29 +1200

    instructions are now shorter

--- streetle-react-project/src/components/Modals/HowToPlayModal.jsx
+++ streetle-react-project/src/components/Modals/HowToPlayModal.jsx
@@ -5,6 +5,7 @@ import { RiCloseLine } from "react-icons/ri";
 const Modal = ({ setIsOpen }) => {
   return (
     <>
+      <div className="overlay"></div>
       <div onClick={() => setIsOpen(false)} />
       <div className="modal">
         <div className="modalHeader">
@@ -16,56 +17,26 @@ const Modal = ({ setIsOpen }) => {
         <div className="modalContent">
           <p>
             <h2>Objective:</h2>
-            Use the street view image to correctly guess which country in the
-            world you are in.
+            utilize Google Street View and other available clues to guess your
+            current location in the world within 5 attempts.
           </p>
           <p>
             <h2>Gameplay:</h2>
-            - Look closely at the street view image to gather clues about the
-            countries location.
-            <br />
-            - Type your guess into the input area at the bottom of the screen.
-            <br />
-            - Click the "Guess" button to submit your guess.
-            <br />
-            - You have 5 guesses to correctly guess the country and win the
-            round.
-            <br />
-            - Each incorrect guess outputs useful data into the legend in the
-            top right corner of the screen.
-            <br />- The legend displays the following information:
-            <ul>
-              <li>The name of the country you guessed.</li>
-              <li>
-                The direction from your guessed country to the correct country.
-              </li>
-              <li>
-                The distance between your guessed country and the correct
-                country in kilometers.
-              </li>
-              <li>
-                The first icon represents the continent (Is your guess in the
-                correct continent?)
-              </li>
-              <li>
-                The second icon represents bordering countries (Does your guess
-                share a border with the correct country?)
-              </li>
-              <li>
-                The third icon represents the hemisphere (Have you guessed in
-                the correct hemisphere?)
-              </li>
-            </ul>
-            - Icons will turn green if your guess matches the attributes of the
-            icons.
-          </p>
-          <p>
+            After each guess, you will receive distance, direction, continent,
+            border, and hemisphere information to assist you in triangulating
+            your current location.
+            <p className="example-text">Example:</p>
+            <div className="img-cont">
+              <img src="htp-legend.png" alt="how to play legend example" />
+            </div>
+            <p>
+              In the above example we successfully guessed Iceland by using the
+              previous clues.
+            </p>
             <h2>Tips:</h2>
-            - Pay close attention to the details in the street view image.
-            <br />
-            - Use the legend to guide your next guess.
-            <br />- Take note of the information displayed in the legend to help
-            you make more informed guesses.
+            Take advantage of the additional information provided by Google
+            Street View, such as signs, architecture, language, and other visual
+            cues.
           </p>
           <p>
             <h2 className="footNote">
